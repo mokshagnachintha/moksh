7542bba2b2b428807131901c57815e268817ab3a
mokshagnachintha <c.mokshagna@gmail.com>
2026-02-27 23:52:36 +0530

    Fix: Switch annotationProcessor to kapt for Room compilation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
@@ -62,5 +63,5 @@ dependencies {
     // Room Database
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
 }
